2e9da7f8d8bee1cdf8c4b9c51502701ce5396289
Allan Muniz <146678270+allmuniz@users.noreply.github.com>
2024-04-04 12:04:34 -0300

    Integreted Docker

--- src/main/java/com/project/certification/modules/students/entities/StudentEntity.java
+++ src/main/java/com/project/certification/modules/students/entities/StudentEntity.java
@@ -1,8 +1,12 @@
 package com.project.certification.modules.students.entities;
 
-import java.util.List;
 import java.util.UUID;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,9 +14,15 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "TB_ESTUDENTS")
 public class StudentEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+    
+    @Column(unique = true,nullable = false)
     private String email;
-    private List<CertificationStudentEntity> certificationStudentEntities;
+
+    // private List<CertificationStudentEntity> certificationStudentEntities;
 }
