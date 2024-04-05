1cd128378d2f600bd47e25130240586a2b245b37
Allan Muniz <146678270+allmuniz@users.noreply.github.com>
2024-04-05 10:39:42 -0300

    Update QuestionController.java

--- src/main/java/com/project/certification/modules/questions/controllers/QuestionController.java
+++ src/main/java/com/project/certification/modules/questions/controllers/QuestionController.java
@@ -1,7 +1,6 @@
 package com.project.certification.modules.questions.controllers;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
